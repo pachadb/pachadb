4ad11e51eb6ce7765c43021682f0ea8721c283bd
Leandro Ostera <leandro@abstractmachines.dev>
2023-08-03 22:29:39 +0200

    parse symbols, variables, and lists

--- nanolog/src/parser.rs
+++ nanolog/src/parser.rs
@@ -5,7 +5,7 @@ lalrpop_mod!(grammar);
 #[cfg(test)]
 mod tests {
     use super::*;
-use crate::ast::*;
+    use crate::ast::*;
 
     macro_rules! parse {
         ($e:expr) => {
@@ -27,5 +27,36 @@ use crate::ast::*;
         assert_eq!(parse!("?hello"), Expr::Variable("?hello".to_string()));
         assert_eq!(parse!("?0"), Expr::Variable("?0".to_string()));
         assert_eq!(parse!("?heLo1"), Expr::Variable("?heLo1".to_string()));
+    }
+
+    #[test]
+    fn parse_lists() {
+        assert_eq!(
+            parse!("(?who likes metallica)"),
+            Expr::List(vec![
+                Expr::Variable("?who".to_string()),
+                Expr::Symbol("likes".to_string()),
+                Expr::Symbol("metallica".to_string())
+            ])
+        );
+
+        assert_eq!(
+            parse!(
+                r#"( ?who likes ?what
+                     ?what is-a band
+                     ?what playsGenre "Heavy Metal" )"#
+            ),
+            Expr::List(vec![
+                Expr::Variable("?who".to_string()),
+                Expr::Symbol("likes".to_string()),
+                Expr::Variable("?what".to_string()),
+                Expr::Variable("?what".to_string()),
+                Expr::Symbol("is-a".to_string()),
+                Expr::Symbol("band".to_string()),
+                Expr::Variable("?what".to_string()),
+                Expr::Symbol("playsGenre".to_string()),
+                Expr::Symbol("Heavy Metal".to_string())
+            ])
+        );
     }
 }
