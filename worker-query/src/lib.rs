0be51a4d49ec489670b6cb8a5292d204a0a987d4
Leandro Ostera <leandro@abstractmachines.dev>
2023-08-13 04:37:53 +0200

    feat: make core IO agnostic
    To be able to run PachaDB in several places (in-browser, deno,
    cloudflare workers, rust projects, standalone, etc) it makes sense to
    detach how it works from the underlying IO layer.
    
    To do this I've introduced a few interfaces that help plug in different
    parts of the IO layer as needed, and refactor the intake and query
    workers to start using them.
    
    I haven't tested this yet, but it at least compiles.
    
    This also splits the Query stage into a Planner and an Executor, that we
    can use for optimizing whatever plan we come up with in the future, and
    for figuring out how to execute a query more efficiently (eg. parallely)
    depending on the IO we're on.
    
    As an example, on Cloudflare Workers we wouldn't be able to massively
    parallelize by having a worker reach out to N workers, because there's
    up to 6 concurrent fetch requests allowed.
    
    However, locally, that would be fine.

--- worker-query/src/lib.rs
+++ worker-query/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate console_error_panic_hook;
+use async_trait::async_trait;
 use log::*;
 use pachadb_core::*;
 use pachadb_nanolog::engine::*;
@@ -8,12 +9,41 @@ use std::panic;
 use worker::kv::KvStore;
 use worker::*;
 
-#[event(queue)]
-async fn handle_event(_batch: MessageBatch<Uri>, _env: Env, _ctx: Context) -> Result<()> {
-    panic::set_hook(Box::new(console_error_panic_hook::hook));
-    wasm_logger::init(wasm_logger::Config::default());
+pub struct CloudflareIndexStore {
+    kv: KvStore,
+}
 
-    Ok(())
+impl CloudflareIndexStore {
+    pub fn new(env: &Env, kv: &str) -> Result<Self> {
+        let kv = env.kv(kv)?;
+        Ok(Self { kv })
+    }
+}
+
+#[async_trait(?Send)]
+impl IndexStore for CloudflareIndexStore {
+    async fn scan(&self, prefix: &str) -> PachaResult<Box<dyn Iterator<Item = IndexKey>>> {
+        let list = self
+            .kv
+            .list()
+            .prefix(prefix.to_string())
+            .execute()
+            .await
+            .map_err(|err| PachaError::UnrecoverableStorageError(err.to_string()))?;
+
+        Ok(Box::new(
+            list.keys.into_iter().map(|key| key.name.parse().unwrap()),
+        ))
+    }
+
+    //NOTE(@ostera): make sure this retunrs an option
+    async fn get(&self, key: IndexKey) -> PachaResult<Option<Fact>> {
+        self.kv
+            .get(&key.to_string())
+            .json::<Fact>()
+            .await
+            .map_err(|err| PachaError::UnrecoverableStorageError(err.to_string()))
+    }
 }
 
 #[event(fetch)]
@@ -23,87 +53,118 @@ async fn handle_request(req: Request, env: Env, _ctx: Context) -> Result<Respons
 
     let router = Router::new();
 
+    // 0..1..2..X..4 -> monotonic reads / read your writes / serializability
+
+    // 	InMemory
+    // 	FileSystem
+    // 	EdgeWorkerCloudlfare <- monotonic reads + snapshot isolation
+    // 		KV
+
     router
         .post_async("/", |mut req, ctx| async move {
             let query_req: QueryReq = req.json().await?;
 
+            let index_scanner = IndexScanner::new(
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-entity")?,
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-entity-field")?,
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-entity-field")?,
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-field")?,
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-field-value")?,
+                CloudflareIndexStore::new(&ctx.env, "pachadb-facts-index-by-value")?,
+            );
+            let query_executor = DefaultQueryExecutor::new(index_scanner);
+            let query_planner = DefaultQueryPlanner::default();
+
+            let query_plan = query_planner
+                .plan_query(query_req.query, query_req.tx_id)
+                .await
+                .map_err(|err| Error::RustError(err.to_string()))?;
+
+            let result = query_executor
+                .run_query_plan(query_plan)
+                .await
+                .map_err(|err| Error::RustError(err.to_string()))?;
+
             // NOTE(@ostera): helps ensure read-your-writes by forcing replication of facts
-            let tx_bucket = ctx.env.kv("pachadb-tx-store")?;
-            let mut forced_fact_ids = vec![];
-            for tx_id in 0..query_req.tx_id.0 {
-                let tx = tx_bucket
-                    .get(&tx_id.to_string())
-                    .json::<Transaction>()
-                    .await?
-                    .ok_or(worker::Error::RustError(format!(
-                        "missing transaction {}",
-                        tx_id
-                    )))?;
-                forced_fact_ids.extend(tx.fact_ids);
-            }
+            // let tx_bucket = ctx.env.kv("pachadb-tx-store")?;
+            // let mut forced_fact_ids = vec![];
+            // for tx_id in 0..query_req.tx_id.0 {
+            //     let tx = tx_bucket
+            //         .get(&tx_id.to_string())
+            //         .json::<Transaction>()
+            //         .await?
+            //         .ok_or(worker::Error::RustError(format!(
+            //             "missing transaction {}",
+            //             tx_id
+            //         )))?;
+            //     forced_fact_ids.extend(tx.fact_ids);
+            // }
 
-            let query = Parser.parse(&query_req.query).unwrap();
-            info!("Executing {:?}", query);
+            // let query = Parser.parse(&query_req.query).unwrap();
+            // info!("Executing {:?}", query);
 
-            let scans: Vec<Scan> = query
-                .body
-                .iter()
-                .flat_map(|atom| match &atom.relation {
-                    Term::Var(_) => {
-                        let entity = atom.args.get(0).unwrap();
-                        let value = atom.args.get(1).unwrap();
-                        match (entity, value) {
-                            (Term::Var(_), Term::Sym(v)) => vec![Scan::Value(v.clone())],
-                            (Term::Sym(e), Term::Var(_)) => vec![Scan::Entity(e.clone())],
-                            (Term::Sym(e), Term::Sym(v)) => {
-                                vec![Scan::EntityValue(e.clone(), v.clone())]
-                            }
-                            (Term::Var(_), Term::Var(_)) => vec![],
-                        }
-                    }
-                    Term::Sym(f) => {
-                        let entity = atom.args.get(0).unwrap();
-                        let value = atom.args.get(1).unwrap();
-                        match (entity, value) {
-                            (Term::Var(_), Term::Sym(v)) => {
-                                vec![Scan::FieldValue(f.clone(), v.clone())]
-                            }
-                            (Term::Sym(e), Term::Var(_)) => {
-                                vec![Scan::EntityField(e.clone(), f.clone())]
-                            }
-                            (Term::Sym(_), Term::Sym(_)) => vec![],
-                            (Term::Var(_), Term::Var(_)) => vec![Scan::Field(f.clone())],
-                        }
-                    }
-                })
-                .collect();
-            info!("Performing {} scans...", scans.len());
+            // ?who master-of anakin -> Scan::FieldValue("master-of", "anakin")
+            // ?who master-of anakin -> Scan::FieldValue("master-of", "anakin")
 
-            let mut facts = {
-                let scanner = Scanner {
-                    index_by_entity: ctx.env.kv("pachadb-facts-index-by-entity")?,
-                    index_by_entity_field: ctx.env.kv("pachadb-facts-index-by-entity-field")?,
-                    // TODO(@ostera): we don't have this one!
-                    index_by_entity_value: ctx.env.kv("pachadb-facts-index-by-entity-field")?,
-                    index_by_field: ctx.env.kv("pachadb-facts-index-by-field")?,
-                    index_by_field_value: ctx.env.kv("pachadb-facts-index-by-field-value")?,
-                    index_by_value: ctx.env.kv("pachadb-facts-index-by-value")?,
-                };
+            // let scans: Vec<Scan> = query
+            //     .body
+            //     .iter()
+            //     .flat_map(|atom| match &atom.relation {
+            //         Term::Var(_) => {
+            //             let entity = atom.args.get(0).unwrap();
+            //             let value = atom.args.get(1).unwrap();
+            //             match (entity, value) {
+            //                 (Term::Var(_), Term::Sym(v)) => vec![Scan::Value(v.clone())],
+            //                 (Term::Sym(e), Term::Var(_)) => vec![Scan::Entity(e.clone())],
+            //                 (Term::Sym(e), Term::Sym(v)) => {
+            //                     vec![Scan::EntityValue(e.clone(), v.clone())]
+            //                 }
+            //                 (Term::Var(_), Term::Var(_)) => vec![],
+            //             }
+            //         }
+            //         Term::Sym(f) => {
+            //             let entity = atom.args.get(0).unwrap();
+            //             let value = atom.args.get(1).unwrap();
+            //             match (entity, value) {
+            //                 (Term::Var(_), Term::Sym(v)) => {
+            //                     vec![Scan::FieldValue(f.clone(), v.clone())]
+            //                 }
+            //                 (Term::Sym(e), Term::Var(_)) => {
+            //                     vec![Scan::EntityField(e.clone(), f.clone())]
+            //                 }
+            //                 (Term::Sym(_), Term::Sym(_)) => vec![],
+            //                 (Term::Var(_), Term::Var(_)) => vec![Scan::Field(f.clone())],
+            //             }
+            //         }
+            //     })
+            //     .collect();
+            // info!("Performing {} scans...", scans.len());
 
-                let mut facts = vec![];
-                for scan in scans {
-                    facts.extend(
-                        scanner
-                            .fetch(scan, &forced_fact_ids, query_req.tx_id)
-                            .await?,
-                    );
-                }
-                facts
-            };
-            facts.push(query);
+            // let mut facts = {
+            //     let scanner = Scanner {
+            //         index_by_entity: ctx.env.kv("pachadb-facts-index-by-entity")?,
+            //         index_by_entity_field: ctx.env.kv("pachadb-facts-index-by-entity-field")?,
+            //         // TODO(@ostera): we don't have this one!
+            //         index_by_entity_value: ctx.env.kv("pachadb-facts-index-by-entity-field")?,
+            //         index_by_field: ctx.env.kv("pachadb-facts-index-by-field")?,
+            //         index_by_field_value: ctx.env.kv("pachadb-facts-index-by-field-value")?,
+            //         index_by_value: ctx.env.kv("pachadb-facts-index-by-value")?,
+            //     };
 
-            let result = Solver.solve(facts);
-            info!("Result {:?}", result);
+            //     let mut facts = vec![];
+            //     for scan in scans {
+            //         facts.extend(
+            //             scanner
+            //                 .fetch(scan, &forced_fact_ids, query_req.tx_id)
+            //                 .await?,
+            //         );
+            //     }
+            //     facts
+            // };
+            // facts.push(query);
+
+            // let result = Solver.solve(facts);
+            // info!("Result {:?}", result);
 
             Response::from_json(&result)
         })
